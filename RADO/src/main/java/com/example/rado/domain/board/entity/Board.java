09d06a419917f6ae6401a717b04a05da90d93849
rudeh2926 <dokyungkim2926@gmail.com>
2023-06-11 22:53:59 +0900

    ♻️ :: 필드 네임 지정

--- RADO/src/main/java/com/example/rado/domain/board/entity/Board.java
+++ RADO/src/main/java/com/example/rado/domain/board/entity/Board.java
@@ -1,7 +1,10 @@
 package com.example.rado.domain.board.entity;
 
+import com.example.rado.domain.user.entity.User;
 import jakarta.persistence.*;
 import lombok.*;
+
+import java.net.UnknownServiceException;
 
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
@@ -11,6 +14,7 @@ public class Board {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @Column(name = "content")
     private String content;
 
     @Builder
