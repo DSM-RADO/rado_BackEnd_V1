f6006e97a90c62da7413f03d2af0664a25958b43
rudeh2926 <dokyungkim2926@gmail.com>
2023-06-14 18:43:03 +0900

    ⛔ :: 중단

--- RADO/src/main/java/com/example/rado/domain/board/repository/BoardCommentRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.rado.domain.board.repository;
-
-import com.example.rado.domain.board.entity.BoardComment;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface BoardCommentRepository extends JpaRepository<BoardComment, Long> {
-}
--- RADO/src/main/java/com/example/rado/domain/user/service/exception/AlreadyExistIdException.java
+++
@@ -1,9 +0,0 @@
-package com.example.rado.domain.user.service.exception;
-
-public class AlreadyExistIdException extends RuntimeException{
-    private static final String MESSAGE = "이미 등록된 아이디 입니다";
-
-    public AlreadyExistIdException(){
-        super(MESSAGE);
-    }
-}
