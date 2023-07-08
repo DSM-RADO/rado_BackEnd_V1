56a24d4163250473c6273d85946c9c15d283e02f
rudeh2926 <dokyungkim2926@gmail.com>
2023-07-08 16:24:53 +0900

    .

--- RADO/src/main/java/com/example/rado/global/security/jwt/dto/TokenResponse.java
+++ RADO/src/main/java/com/example/rado/global/security/jwt/dto/TokenResponse.java
@@ -1,11 +1,13 @@
-package com.example.onepiece.User.global.security.jwt.dto;
+package com.example.rado.global.security.jwt.dto;
 
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 
 import java.time.LocalDateTime;
 
 @Getter
+@Builder
 @AllArgsConstructor
 public class TokenResponse {
 
