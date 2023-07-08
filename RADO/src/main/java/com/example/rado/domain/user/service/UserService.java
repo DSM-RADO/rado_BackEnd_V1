6238a96d0dc9cd2a47666e97819a04e74ffc8421
rudeh2926 <dokyungkim2926@gmail.com>
2023-07-08 16:24:22 +0900

    .

--- RADO/src/main/java/com/example/rado/domain/user/service/UserService.java
+++ RADO/src/main/java/com/example/rado/domain/user/service/UserService.java
@@ -1,19 +1,18 @@
 package com.example.rado.domain.user.service;
 
 import com.example.rado.domain.user.controller.dto.request.UserAddRequest;
+import com.example.rado.domain.user.controller.dto.request.UserDuplicate;
 import com.example.rado.domain.user.controller.dto.request.UserLoginRequest;
-import com.example.rado.domain.user.controller.dto.response.TokenResponse;
 import com.example.rado.domain.user.entity.User;
 import com.example.rado.domain.user.repository.UserRepository;
 import com.example.rado.domain.user.service.Faeade.UserFacade;
-import com.example.rado.global.security.jwt.JwtProperties;
-import com.example.rado.global.security.jwt.JwtTokenProvider;
+import com.example.rado.global.security.jwt.dto.TokenResponse;
+import com.example.rado.global.security.jwt.repository.JwtProvider;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
@@ -21,52 +20,39 @@ public class UserService {
 
     private final UserRepository userRepository;
     private final UserFacade userFacade;
-    private final JwtProperties jwtProperties;
-    private final JwtTokenProvider jwtTokenProvider;
+    private final JwtProvider jwtProvider;
     private final PasswordEncoder passwordEncoder;
 
     @Transactional
-    public void addUser(UserAddRequest request){
-        String userId = request.getUserId();
-        Optional<User> user = userRepository.findByUserId(userId);
-        if (user.isEmpty()){
-            userRepository.save(
-                    User.builder()
-                            .userId(request.getUserId())
-                            .userPassword(request.getUserPassword())
-                            .userName(request.getUserName())
-                            .year(request.getYear())
-                            .month(request.getMonth())
-                            .day(request.getDay())
-                            .build());
-        }
-        else {
-            throw new IllegalArgumentException("중복된 아이디 입니다");
-        }
+    public void addUser(UserAddRequest request) {
+        userRepository.save(
+                User.builder()
+                        .userId(request.getUserId())
+                        .userPassword(request.getUserPassword())
+                        .userName(request.getUserName())
+                        .year(request.getYear())
+                        .month(request.getMonth())
+                        .day(request.getDay())
+                        .build());
     }
 
-    public TokenResponse userLogin(UserLoginRequest request){
+    public boolean duplicateUser(UserDuplicate userDuplicate){
+            return userFacade.existsByUserId(userDuplicate.getUserId());
+    }
 
-        User user = userRepository.findByUserId(request.getUserId())
-                .orElseThrow();
+    public TokenResponse login(UserLoginRequest userLoginRequest){
+        User user = userFacade.getUserByUserId(userLoginRequest.getUserId());
 
-        if (request.getUserId().equals(user.getUserId()) && (request.getUserPassword().equals(user.getUserPassword()))){
-            return TokenResponse
-                    .builder()
-                    .accessToken(jwtTokenProvider.generateAccessToken(user.getUserId()))
-                    .expiredAt(java.time.LocalDateTime.now()
-                            .plusSeconds(jwtProperties.getAccessExpiration()))
+        String accessToken = jwtProvider.generateAccessToken(user.getUserId());
+
+        if (passwordEncoder.matches(userLoginRequest.getUserPassword(), user.getUserPassword()) && userLoginRequest.getUserId() == user.getUserId()){
+            return TokenResponse.builder()
+                    .accessToken(accessToken)
                     .build();
         }
         else {
-            throw new IllegalArgumentException("로그인 실패");
+            throw new IllegalArgumentException("아이디 또는 비밀번호가 일치하지 않습니다");
         }
-    }
-
-    @Transactional
-    public void userRemove(){
-        User user = userFacade.currentUser();
-        userRepository.delete(user);
     }
 
 }
