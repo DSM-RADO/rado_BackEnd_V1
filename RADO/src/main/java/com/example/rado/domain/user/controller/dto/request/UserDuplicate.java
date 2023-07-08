f1df24223dca7b1f6faaded0cfc885dd2c10aa3c
rudeh2926 <dokyungkim2926@gmail.com>
2023-07-08 16:24:09 +0900

    .

--- RADO/src/main/java/com/example/rado/domain/user/controller/dto/request/UserDuplicate.java
+++ RADO/src/main/java/com/example/rado/domain/user/controller/dto/request/UserDuplicate.java
@@ -1,2 +1,11 @@
-package com.example.rado.domain.user.controller.dto.request;public class UserDuplicate {
+package com.example.rado.domain.user.controller.dto.request;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+public class UserDuplicate {
+
+    private String userId;
 }
